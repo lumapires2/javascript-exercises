e592b23f8683ae2cd0a02189baba5ba15e665322
Luma Pires <luma.pires@opwell.ai>
2025-02-14 16:26:28 -0300

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, n_times) {
+    let repeated_string = ''
+    if (n_times < 0) {return 'ERROR'}
+    for (i=1; i<n_times+1; i++) {
+        repeated_string += string
+    }
+    return repeated_string
 };
 
 // Do not edit below this line
