e1c31b624426848f6dd12907b72eec30692d165a
Luma Pires <luma.pires@opwell.ai>
2025-02-17 08:07:38 -0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...manyMoreArgs) {
+    for (i of manyMoreArgs) {
+        idx = arr.indexOf(i);
+        while (idx != -1) {
+            arr = arr.toSpliced(idx, 1);
+            idx = arr.indexOf(i);
+        }        
+    }
+    return arr
 };
 
 // Do not edit below this line
