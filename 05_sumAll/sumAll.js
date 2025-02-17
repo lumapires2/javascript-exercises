ab8d54aecf0d9d0ab831f8d1a4ba3e4a2cec662c
Luma Pires <luma.pires@opwell.ai>
2025-02-17 08:19:06 -0300

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (!(Number.isInteger(int1) && Number.isInteger(int2)) || 
+    !((int1 > 0) && (int2>0))) {
+        return "ERROR"
+    }
+    lower = Math.min(int1, int2)
+    higher = Math.max(int1, int2)
+    let sum = 0
+    for (i=lower; i<=higher; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
