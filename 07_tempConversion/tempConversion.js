875eb748feab594d910902b11635a7907ddf75da
Luma Pires <luma.pires@opwell.ai>
2025-02-17 08:45:59 -0300

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  if (!typeof(temperature === "number")) {
+    return 'ERROR'
+  }
+  celsius = (temperature - 32)*5/9
+  if (celsius % 1 == 0) {
+    return Math.floor(celsius)}
+  else {
+    return Number(celsius.toFixed(1))}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  if (!typeof(temperature === "number")) {
+    return 'ERROR'
+  }
+  fahrenheit = (temperature*9/5) + 32
+  if (fahrenheit % 1 == 0) {
+    return Math.floor(fahrenheit)}
+  else {
+    return Number(fahrenheit.toFixed(1))}
 };
 
 // Do not edit below this line
