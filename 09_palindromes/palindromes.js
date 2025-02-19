fc8f8410e621dbcf858237e693ab42d10295cc91
Luma Pires <luma.pires@opwell.ai>
2025-02-19 17:40:44 -0300

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9 ]/g, "")
+    str = str.toLowerCase()
+    str = str.replace(/\s/g, "")
+    for (i=0; i<Math.floor(str.length/2); i++) {
+        if (str[i] != str[str.length - i - 1]) {
+            return false
+        }
+    }
 
+    return true
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,5 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    return n == 1 ? 1 : n == 2 ? 1 : fibonacci(n-2) + fibonacci(n-1)
 };
 
 // Do not edit below this line
