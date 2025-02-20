96f06644b506e1b9fc9a26d98093fc06bfd459f9
Luma Pires <luma.pires@opwell.ai>
2025-02-20 09:16:35 -0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
 const fibonacci = function(n) {
-    return n == 1 ? 1 : n == 2 ? 1 : fibonacci(n-2) + fibonacci(n-1)
+    n = parseInt(n);
+    return n<0? "OOPS": n == 0? 0 : n == 1 ? 1 : n == 2 ? 1 : fibonacci(n-2) + fibonacci(n-1)
 };
 
 // Do not edit below this line
